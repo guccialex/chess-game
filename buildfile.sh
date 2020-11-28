37fca4750585da4b4e5957b699a7bf9d31a7b69e
alexander <alex.gusain@gmail.com>
2020-11-28 16:34:16 -0500

    test commit

---
+++ buildfile.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+
+
+#what I think my run script will/should end up as
+
+#build the chesspoker package
+cd chesspoker_package
+# dont think i need to build it here
+#cargo build
+
+cd ..
+cd wasm_builder
+
+#build the wasm package with the target of web
+wasm-pack build --target web
+
+cd pkg
+
+#copy the package created into the frontend wasm file directory
+cp wasm_builder.js ../../chesscheckersgame/wasmfiles
+cp wasm_builder_bg.wasm ../../chesscheckersgame/wasmfiles
+
+#copy the chesspoker game to the server
+cd ..
+cd ..
+cp ./chesscheckersgame ./express_server/public -r
+
+#run the server
+cd express_server
+npm run start
